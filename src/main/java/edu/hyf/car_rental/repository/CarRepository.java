e6c2e53219b22808c013b184cf3cb2ee2b9a3382
boba-milktea <catherine.idv@gmail.com>
2026-04-28 15:04:25 +0200

    add model and repo

---
+++ src/main/java/edu/hyf/car_rental/repository/CarRepository.java
@@ -0,0 +1,15 @@
+package edu.hyf.car_rental.repository;
+
+
+import edu.hyf.car_rental.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+
+public interface CarRepository extends JpaRepository <Car, Long>{
+    boolean existsById(Long id);
+    Optional<Car>findById(Long id);
+}
